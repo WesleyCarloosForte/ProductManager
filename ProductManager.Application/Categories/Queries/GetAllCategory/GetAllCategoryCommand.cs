6c371a32edf89915b6373d52cb6e934eca20c0c5
wesley <wesley>
2025-06-18 18:15:44 -0300

    Creacion de CRQS Para listar todas las categorias

---
+++ ProductManager.Application/Categories/Queries/GetAllCategory/GetAllCategoryCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using ProductManager.Application.Common.Response;
+using ProductManager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManager.Application.Categories.Queries.GetAllCategory
+{
+    public record GetAllCategoryCommand():IRequest<Result<IEnumerable<Category>>>;
+
+}
---
+++ ProductManager.Application/Categories/Queries/GetAllCategory/GetAllCategoryHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using ProductManager.Application.Common.Response;
+using ProductManager.Domain.Entities;
+using ProductManager.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManager.Application.Categories.Queries.GetAllCategory
+{
+    public class GetAllCategoryHandler : IRequestHandler<GetAllCategoryCommand, Result<IEnumerable<Category>>>
+    {
+        private readonly IRepository<Category> _categoryRepository;
+        public GetAllCategoryHandler(IRepository<Category> categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<Result<IEnumerable<Category>>> Handle(GetAllCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _categoryRepository.GetAllAsync();
+
+            return Result<IEnumerable<Category>>.Success(result);
+        }
+    }
+}
