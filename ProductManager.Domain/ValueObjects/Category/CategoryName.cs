8b2624c8800dd567c87b4267354ab33d86af770b
wesley <wesley>
2025-06-18 18:14:20 -0300

    refinando validaciones del value object categoryName

--- ProductManager.Domain/ValueObjects/Category/CategoryName.cs
+++ ProductManager.Domain/ValueObjects/Category/CategoryName.cs
@@ -7,6 +7,13 @@ namespace ProductManager.Domain.ValueObjects.Category
         public CategoryName(string value)
         {
             if(string.IsNullOrEmpty(value)) throw new ArgumentNullException("Category name is required.");
+           
+            else if (value.Length < 4)
+                throw new ArgumentException("Category name must be at least 4 characters long.");
+
+            else if (value.Length > 100)
+                throw new ArgumentException("Category name must not exceed 100 characters.");
+
             Value = value;
         }
         public bool Equals(CategoryName? other) => other!=null && Value == other.Value;
