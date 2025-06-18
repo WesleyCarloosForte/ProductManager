0649ea29e682b4ab55b2ae024f663fd921a7499f
wesley <wesley>
2025-06-18 18:13:46 -0300

    Agregando constructor y metodo Create a categoria

--- ProductManager.Domain/Entities/Category.cs
+++ ProductManager.Domain/Entities/Category.cs
@@ -4,13 +4,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ProductManager.Domain.Entities
 {
-    public class Category:BaseEntity
+    public class Category : BaseEntity
     {
-        public CategoryName Name {  get; set; }
-        public ICollection<Product> Products {  get; set; } 
+        public CategoryName Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+
+        public Category(CategoryName name) 
+        {
+            Name =name;
+        }
+        public Category() 
+        {
+        
+        }
+
+        public static Category Create(string name) => new Category { Name = CategoryName.Create(name) };
     }
 }
