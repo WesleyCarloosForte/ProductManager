dbcf81148c1e76aff17abf524b8e00b861b91998
wesley <wesley>
2025-06-19 22:12:29 -0300

    moviendo enum de estados a la carpeta correspondiente

---
+++ ProductManager.Domain/Common/Enums/EntityStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManager.Domain.Common.Enums
+{
+    public enum EntityStatus
+    {
+        Enabled =1,
+        Disabled=2,
+        Deleted=3
+    }
+}
