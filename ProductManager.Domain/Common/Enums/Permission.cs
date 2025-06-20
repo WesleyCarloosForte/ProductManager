f874cf170debb110666e2ec9537a13e2da5000b4
wesley <wesley>
2025-06-19 22:13:00 -0300

    creacion de enum que representan los permisos de usuarios

---
+++ ProductManager.Domain/Common/Enums/Permission.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManager.Domain.Common.Enums
+{
+    public enum Permission
+    {
+        Product_Read,
+        Product_Write,
+        Product_Delete,
+        Category_Read,
+        Category_Write,
+        Category_Delete
+    }
+}
